46443f62b20fdd7172bc8eb3f9b2034fe0a7a74b
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2018-05-17 16:24:55 +0700

    Added a map to app

---
+++ src/components/MapCanvas.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { compose, withProps } from 'recompose';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
+
+const MapCanvas = compose(
+  withProps({
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?AIzaSyCejQoXsMW6FestdxLCiM4uCny49qz5rHc&v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `90vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    <Marker position={{ lat: -34.397, lng: 150.644 }} />
+  </GoogleMap>
+));
+
+export default MapCanvas;
---
+++ src/containers/Map.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MapCanvas from '../components/MapCanvas.js';
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div>
+        Map Template
+        <MapCanvas />
+      </div>
+    );
+  }
+}
+
+export default Map;
