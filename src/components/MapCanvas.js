2c449ad01f0500db49f9fd837d466e578d7f45b4
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2018-05-17 22:19:40 +0700

    Styled the map

--- src/components/MapCanvas.js
+++ src/components/MapCanvas.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { compose, withProps } from 'recompose';
+import React from "react";
+import { compose, withProps } from "recompose";
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker
-} from 'react-google-maps';
+} from "react-google-maps";
+import MapStyles from "../utils/MapStyles";
 
 const MapCanvas = compose(
   withProps({
     googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?AIzaSyCejQoXsMW6FestdxLCiM4uCny49qz5rHc&v=3.exp&libraries=geometry,drawing,places',
+      "https://maps.googleapis.com/maps/api/js?AIzaSyCejQoXsMW6FestdxLCiM4uCny49qz5rHc&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `80vh` }} />,
     mapElement: <div style={{ height: `100%` }} />
@@ -18,7 +19,11 @@ const MapCanvas = compose(
   withScriptjs,
   withGoogleMap
 )(props => (
-  <GoogleMap defaultZoom={13} defaultCenter={{ lat: 56.488, lng: 84.98 }}>
+  <GoogleMap
+    defaultZoom={13}
+    defaultCenter={{ lat: 56.488, lng: 84.98 }}
+    defaultOptions={{ styles: MapStyles }}
+  >
     <Marker position={{ lat: 56.488, lng: 84.98 }} />
   </GoogleMap>
 ));
