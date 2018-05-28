0dc330e65b379b87354eddd1ecd7f7e6bba38db3
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2018-05-28 15:01:44 +0700

    FEAT: made calls from Foursquare API

--- src/components/MapCanvas.js
+++ src/components/MapCanvas.js
@@ -11,21 +11,68 @@ import MapStyles from "../utils/MapStyles";
 const MapCanvas = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?AIzaSyCejQoXsMW6FestdxLCiM4uCny49qz5rHc&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `80vh` }} />,
-    mapElement: <div style={{ height: `100%` }} />
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCejQoXsMW6FestdxLCiM4uCny49qz5rHc&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: (
+      <div
+        style={{
+          height: `100%`
+        }}
+      />
+    ),
+    containerElement: (
+      <div
+        style={{
+          height: `80vh`
+        }}
+      />
+    ),
+    mapElement: (
+      <div
+        style={{
+          height: `100%`
+        }}
+      />
+    )
   }),
   withScriptjs,
   withGoogleMap
-)(props => (
-  <GoogleMap
-    defaultZoom={13}
-    defaultCenter={{ lat: 56.488, lng: 84.98 }}
-    defaultOptions={{ styles: MapStyles }}
-  >
-    <Marker position={{ lat: 56.488, lng: 84.98 }} />
-  </GoogleMap>
-));
+)(props => {
+  // let markers = props.places.map(place => {
+  //   return (
+  //     <Marker
+  //       position={{
+  //         lat: place.location.lat,
+  //         lng: place.location.lng
+  //       }}
+  //       key={place.id}
+  //     />
+  //   );
+  // });
+
+  return (
+    <GoogleMap
+      defaultZoom={13}
+      defaultCenter={{
+        lat: 56.488,
+        lng: 84.98
+      }}
+      defaultOptions={{
+        styles: MapStyles
+      }}
+    >
+      {props.places.map(place => {
+        return (
+          <Marker
+            position={{
+              lat: place.location.lat,
+              lng: place.location.lng
+            }}
+            key={place.id}
+          />
+        );
+      })}
+    </GoogleMap>
+  );
+});
 
 export default MapCanvas;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.baseURL = "https://api.foursquare.com/v2/venues";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
